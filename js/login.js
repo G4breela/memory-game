7efd4e860ce673f0412cb131ae41ab006df9feca
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-02-17 17:13:10 -0300

    Update login.js
    formatando

--- js/login.js
+++ js/login.js
@@ -3,15 +3,15 @@ const button = document.querySelector(".login-button");
 
 // função do evento no parametro fazemos referencia ao objeto
 const validadeInput = ({ target }) => {
-  // se o tamanho do valor do input for maior que 2
-  if (target.value.length > 2) {
-    // removemos o atributo disable do botão
-    button.removeAttribute("disabled");
-    // para a execução e pula direto para o final da função neste caso
-    return;
-  }
-  // se não for maior adicionamos o atributo disable novamente ao botão sem precisar usar o else
-  button.setAttribute("disabled", "");
+    // se o tamanho do valor do input for maior que 2
+    if (target.value.length > 2) {
+        // removemos o atributo disable do botão
+        button.removeAttribute("disabled");
+        // para a execução e pula direto para o final da função neste caso
+        return;
+    }
+    // se não for maior adicionamos o atributo disable novamente ao botão sem precisar usar o else
+    button.setAttribute("disabled", "");
 };
 
 // adicionando o ouvinte no input para excecutar a função
