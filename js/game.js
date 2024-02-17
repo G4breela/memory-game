dfd5ee6396e82eae0591926a3f76874b71ec26c3
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-02-17 18:19:15 -0300

    Update game.js
    formatando

--- js/game.js
+++ js/game.js
@@ -1,26 +1,26 @@
-const grid = document.querySelector('.grid');
+const grid = document.querySelector(".grid");
 
- // estamos criando elementos div através do js e como parametros vamos ter a tag e a class que o elemento vai receber
+// estamos criando elementos div através do js e como parametros vamos ter a tag e a class que o elemento vai receber
 const createElement = (tag, className) => {
-    const element = document.createElement(tag);
-    element.className = className;
-    return element;
-}
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+};
 
 const createCard = () => {
-   // então vamos criar um elemento div com a class card
-    const card = createElement('div', 'card');
+  // então vamos criar um elemento div com a class card
+  const card = createElement("div", "card");
 
-    // a mesma coisa aqui, vamos criar um elemento div com a class face front
-    const front = createElement('div', 'face front');
+  // a mesma coisa aqui, vamos criar um elemento div com a class face front
+  const front = createElement("div", "face front");
 
-    // a mesma coisa aqui, vamos criar um elemento div com a class face back
-    const back = createElement('div', 'face back');
+  // a mesma coisa aqui, vamos criar um elemento div com a class face back
+  const back = createElement("div", "face back");
 
-    // para criar a mesma estrutura que temos no html precisamos adicionar o front e o back dentro da div card
-    // utilizando o appendChild();
-    card.appendChild(front);
-    card.appendChild(back);
+  // para criar a mesma estrutura que temos no html precisamos adicionar o front e o back dentro da div card
+  // utilizando o appendChild();
+  card.appendChild(front);
+  card.appendChild(back);
 
-    return card;
+  return card;
 };
