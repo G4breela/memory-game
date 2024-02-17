6ff8d1542a143574e717cefbdd9209b7c523b576
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-02-17 19:48:44 -0300

    Update game.js
    criamos a função que revela a carta
    
    depois adicionamos um atributo com o nome do personagem retornado no array
    
    com esse atributo criamos uma função que verifica se esses atributos são iguais e adiciona uma class desabilitando o card
    
    por fim uma função que verifica se o jogo chegou ao fim exibindo um alert

--- js/game.js
+++ js/game.js
@@ -2,12 +2,12 @@ const grid = document.querySelector(".grid");
 
 // essa array recebe o mesmo valor que o nome das imagens que vão ficar no front
 const characters = [
-    'abelinha',
-    'cachorrinho',
-    'coelinho',
-    'gatinho',
-    'porquinho',
-    'ursinho'
+  "abelinha",
+  "cachorrinho",
+  "coelinho",
+  "gatinho",
+  "porquinho",
+  "ursinho",
 ];
 // estamos criando elementos div através do js e como parametros vamos ter a tag e a class que o elemento vai receber
 const createElement = (tag, className) => {
@@ -16,16 +16,67 @@ const createElement = (tag, className) => {
   return element;
 };
 
-// essa função adiciona a class que criamos no css
-const reavelCard = ({target}) => {
+let firstCard = "";
+let secondCard = "";
 
-    // esse if evita que uma carta que já tenha a class receba a classe novamente
-    if(target.parentNode.className.includes('reveal-card')){
-        return;
+const checkEndGame = () => {
+    // verificando todos os elementos que possuem a class disabled-cards
+    const disabledCards = document.querySelectorAll('.disabled-card');
+
+    // se o tamanho for igual a 12 acaba o jogo
+    if(disabledCards.length === 12){
+        alert('Parabéns, você conseguiu!');
     }
+};
 
+const checkCards = () => {
+  // pegamos o atributo que foi adicionado na função de criar o card
+  const firstCharacter = firstCard.getAttribute("data-character");
+  const secondCharacter = secondCard.getAttribute("data-character");
+
+  // verifica se são iguais
+  if (firstCharacter === secondCharacter) {
+    // se for o card é desabilitado com a class do css
+    firstCard.firstChild.classList.add("disabled-card");
+    secondCard.firstChild.classList.add("disabled-card");
+
+    // e remove o valor da variavel para ser possivel clicar novamente
+    firstCard = "";
+    secondCard = "";
+
+    checkEndGame();
+  } else {
+    // se as cartas não forem iguais remove a classe que revela em 500ms
+    setTimeout(() => {
+      firstCard.classList.remove("reveal-card");
+      secondCard.classList.remove("reveal-card");
+
+      // aqui deixamos o valor vazio novamente para ser possivel clicar de novo
+      firstCard = "";
+      secondCard = "";
+    }, 500);
+  };
+};
+
+// essa função adiciona a class que criamos no css
+const reavelCard = ({ target }) => {
+  // esse if evita que uma carta que já tenha a class receba a classe novamente
+  if (target.parentNode.className.includes("reveal-card")) {
+    return;
+  }
+
+  // verifica se a primeira carta está vazia
+  if (firstCard == "") {
     // aqui setamos o elemento, mas selecionamos o pai desse elemento (o card) e adiciona a class
-    target.parentNode.classList.add('reveal-card');
+    target.parentNode.classList.add("reveal-card");
+    // adiciona o pai do elemento a variavel
+    firstCard = target.parentNode;
+  } else if (secondCard == "") {
+    target.parentNode.classList.add("reveal-card");
+    secondCard = target.parentNode;
+
+    checkCards();
+  }
 };
 
 const createCard = (character) => {
@@ -36,7 +87,7 @@ const createCard = (character) => {
   // a mesma coisa aqui, vamos criar um elemento div com a class face back
   const back = createElement("div", "face back");
 
-// aqui adicionamos a imagem percorrenco o array e pegando o valor e passando um caminho
+  // aqui adicionamos a imagem percorrenco o array e pegando o valor e passando um caminho
   front.style.backgroundImage = `url(../img/${character}.jpg)`;
 
   // para criar a mesma estrutura que temos no html precisamos adicionar o front e o back dentro da div card
@@ -44,27 +95,29 @@ const createCard = (character) => {
   card.appendChild(front);
   card.appendChild(back);
 
-    // adicionado um ouvinte para que quando o card for clicado executar uma função
-    card.addEventListener('click', reavelCard);
+  // adicionado um ouvinte para que quando o card for clicado executar uma função
+  card.addEventListener("click", reavelCard);
+
+  // adicionando um atributo com o valor do nome do personagem
+  card.setAttribute("data-character", character);
 
   return card;
 };
 
 // função que carrega o jogo
 const loadGame = () => {
+  // um array que vai espalhar os personagens do array de personagens duplicando
+  const duplicateCharacters = [...characters, ...characters];
+  // embaralhando as cartas com o array duplicado utilizando o sort() com uma função que retorna um valor de math.random() - 0.5
+  const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
 
-    // um array que vai espalhar os personagens do array de personagens duplicando
-    const duplicateCharacters = [...characters, ...characters];
-    // embaralhando as cartas com o array duplicado utilizando o sort() com uma função que retorna um valor de math.random() - 0.5
-    const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
-
-    // pega o array de personagens e para cada personagem, ou seja, cada item
-    shuffledArray.forEach((character) => {
-        // a função createCard é executada com um personagem
-        const card = createCard(character);
-        // e adiciona o card criado dentro do nosso elementro grid de card 
-        grid.appendChild(card);
-    });
-}
+  // pega o array de personagens e para cada personagem, ou seja, cada item
+  shuffledArray.forEach((character) => {
+    // a função createCard é executada com um personagem
+    const card = createCard(character);
+    // e adiciona o card criado dentro do nosso elementro grid de card
+    grid.appendChild(card);
+  });
+};
 
 loadGame();
