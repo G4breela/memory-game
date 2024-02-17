0bf9733559aefb497630ccd7caf6dde89a304135
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-02-17 17:27:28 -0300

    Update login.js
    criando a função de submit que vai salvar o valor do input no localstorage e depois redirecionar o usuário para a pagina do game

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 const input = document.querySelector(".login-input");
 const button = document.querySelector(".login-button");
+const form = document.querySelector(".login-form");
 
 // função do evento no parametro fazemos referencia ao objeto
 const validadeInput = ({ target }) => {
@@ -13,7 +14,18 @@ const validadeInput = ({ target }) => {
   // se não for maior adicionamos o atributo disable novamente ao botão sem precisar usar o else
   button.setAttribute("disabled", "");
 };
+// adicionando o ouvinte no input para excecutar a função
+const handleSubmit = (e) => {
+  // bloqueando o envio do formulário
+  e.preventDefault();
+  // salvando o valor do input no localstorage do browser com a chave player
+  localStorage.setItem("player", input.value);
+  // redirecionando o usuário para a página do game
+  window.location = "pages/game.html";
+};
 
 // adicionando o ouvinte no input para excecutar a função
-
 input.addEventListener("input", validadeInput);
+
+// adicionando o ouvinte no formulario para executar a função
+form.addEventListener("submit", handleSubmit);
