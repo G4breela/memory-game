1d2b203f409f814d3be5e04ffd3781ccbf27f8bd
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-02-17 17:06:31 -0300

    Update login.js
    comentários

--- js/login.js
+++ js/login.js
@@ -1,10 +1,15 @@
 const input = document.querySelector('.login-input');
 const button = document.querySelector('.login-button');
 
+// função do evento no parametro fazemos referencia ao objeto
 const validadeInput = ({target}) => {
+    // se o tamanho do valor do input for maior que 2
     if(target.value.length > 2){
+        // removemos o atributo disable do botão
         button.removeAttribute('disabled');
     }
 }
+
+// adicionando o ouvinte no input para excecutar a função
 
 input.addEventListener('input', validadeInput);
