2ee180e3f5b0c60e1945928d392fdbc557b612f4
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-02-17 18:13:04 -0300

    Update game.js
    criando um card atraves da função, sem precisar criar manualmente no html

--- js/game.js
+++ js/game.js
@@ -1,3 +1,24 @@
-const createCard = () => {
+const grid = document.querySelector('.grid');
 
+const createCard = () => {
+    // estamos criando elementos div através do js
+    const card = document.createElement('div');
+    const front = document.createElement('div');
+    const back = document.createElement('div');
+
+    // aqui adicionamos as classes que existem na div já existente
+    card.className = 'card';
+    front.className = 'face front';
+    back.className = 'face back';
+
+    // para criar a mesma estrutura que temos no html precisamos adicionar o front e o back dentro da div card
+    // utilizando o appendChild();
+    card.appendChild(front);
+    card.appendChild(back);
+
+
+    // aqui vamos adicionar o card com os seus elementos dentro do grid conforme a card já existente
+    grid.appendChild(card);
 };
+
+createCard();
