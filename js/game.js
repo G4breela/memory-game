ff2a4f9c0821b8293c4a2cba901de0b9d6b2580a
Gabriela Silva <103966644+G4breela@users.noreply.github.com>
2024-02-17 18:47:57 -0300

    Update game.js
    criando a função que carrega o jogo,
    
    para cada personagem dentro do array de personagens é criado um card e adiciona esse card dentro da nossa div grid

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 const grid = document.querySelector(".grid");
 
 // essa array recebe o mesmo valor que o nome das imagens que vão ficar no front
-const cards = [
+const characters = [
     'abelinha',
     'cachorrinho',
     'coelinho',
@@ -16,15 +16,16 @@ const createElement = (tag, className) => {
   return element;
 };
 
-const createCard = () => {
+const createCard = (character) => {
   // então vamos criar um elemento div com a class card
   const card = createElement("div", "card");
-
   // a mesma coisa aqui, vamos criar um elemento div com a class face front
   const front = createElement("div", "face front");
-
   // a mesma coisa aqui, vamos criar um elemento div com a class face back
   const back = createElement("div", "face back");
+
+// aqui adicionamos a imagem percorrenco o array e pegando o valor e passando um caminho
+  front.style.backgroundImage = `url(../img/${character}.jpg)`;
 
   // para criar a mesma estrutura que temos no html precisamos adicionar o front e o back dentro da div card
   // utilizando o appendChild();
@@ -33,3 +34,17 @@ const createCard = () => {
 
   return card;
 };
+
+// função que carrega o jogo
+const loadGame = () => {
+
+    // pega o array de personagens e para cada personagem, ou seja, cada item
+    characters.forEach((character) => {
+        // a função createCard é executada com um personagem
+        const card = createCard(character);
+        // e adiciona o card criado dentro do nosso elementro grid de card 
+        grid.appendChild(card);
+    });
+}
+
+loadGame();
